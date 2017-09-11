33514c872bd86e32c735317bb2e2d83c2cf690b2
felix <2319979647@qq.com>
2017-09-11 15:59:02 +0800

    change the progress

--- Examples/Examples/ViewController.m
+++ Examples/Examples/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-#import <YDBluetoothWebBridge/YDBluetoothWebViewMgr.h>
+#import <YDBluetoothWebBridge/YDBridgeWebViewController.h>
+#import <YDBluetoothWebBridge/YDDefine.h>
 
 @interface ViewController ()
 
@@ -17,7 +18,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [btn setTitle:@"S3蓝牙" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(toThirdPartVC:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    btn.frame = CGRectMake(100, 100, 100, 30);
 
+
+}
+
+- (void)toThirdPartVC:(id)sender {
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"YDPeripheralList.html" ofType:nil];
+    NSString *htmlString = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    YDBridgeWebViewController *vc = [YDBridgeWebViewController new];
+    vc.urlString = htmlString;
+    vc.type = YDWebViewTypeBluetooth;
+    vc.bluetoothBusinessType = 0;
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+//for test
+
+- (void)onLoadHtmlNotify:(NSNotification *)noti {
+    NSString *urlString = noti.object;
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:urlString ofType:nil];
+    NSString *htmlString = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    [[NSNotificationCenter defaultCenter] postNotificationName:YDNtfLoadHtml object:htmlString];
 }
 
 
